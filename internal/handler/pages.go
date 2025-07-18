ee370d6a6d6619064a31d0c4f320ee2f9fd43c0b
d1mk9 <d1mk9@ya.ru>
2025-07-18 11:57:00 +0300

    add seo

--- internal/handler/pages.go
+++ internal/handler/pages.go
@@ -17,6 +17,62 @@ func AboutHandler(w http.ResponseWriter, r *http.Request) {
 	renderTemplate(w, "about.html")
 }
 
+func HowToDownloadHandler(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "how-to-download-rutube.html")
+}
+
+func RutubeToMP4Handler(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "rutube-to-mp4.html")
+}
+
+func RutubeAndroidHandler(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "rutube-android-download.html")
+}
+
+func Download2025Handler(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "download-rutube-2025.html")
+}
+
+func RutubePlaylistHandler(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "rutube-playlist-download.html")
+}
+
+func RutubeNoWatermarkHandler(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "rutube-without-watermark.html")
+}
+
+func RutubeIphoneHandler(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "save-rutube-on-iphone.html")
+}
+
+func RutubeWindowsHandler(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "rutube-windows-download.html")
+}
+
+func RutubeSmartTVHandler(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "rutube-smarttv-save.html")
+}
+
+func RutubeShortsHandler(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "rutube-shorts-download.html")
+}
+
+func RutubePrivateHandler(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "rutube-private-download.html")
+}
+
+func RutubeEmbedHandler(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "rutube-embed-download.html")
+}
+
+func TopRutubeHandler(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "top-rutube-videos.html")
+}
+
+func RutubeAdsRemoveHandler(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "rutube-ads-remove.html")
+}
+
 func renderTemplate(w http.ResponseWriter, name string) {
 	tmpl := template.Must(template.ParseFiles("internal/templates/" + name))
 	tmpl.Execute(w, nil)
